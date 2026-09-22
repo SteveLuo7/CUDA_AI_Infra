be187010609d0db06ea33a695b472e9807f532b0
SteveLuo7 <291581211@qq.com>
2026-09-22 11:25:39 +0800

    D1.1

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My name is Steve" << std::endl;
+    std::cout << "My age is  " << 18 << std::endl;
+    std::cout << "I am learning C++" << std::endl;
+
+    return 0;}
